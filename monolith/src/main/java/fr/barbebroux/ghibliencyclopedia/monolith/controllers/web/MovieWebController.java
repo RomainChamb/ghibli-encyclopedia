b530868ea29c7fe2c56f9cc8f036ea0e028585c3
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2025-11-04 22:26:53 +0100

    Rename Controller and html pages

--- monolith/src/main/java/fr/barbebroux/ghibliencyclopedia/monolith/controllers/web/MovieWebController.java
+++ monolith/src/main/java/fr/barbebroux/ghibliencyclopedia/monolith/controllers/web/MovieWebController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TodoWebController {
+public class MovieWebController {
 
-    @GetMapping("/todos")
+    @GetMapping("/movies")
     public String todos() {
-        return "todos.html";
+        return "movies.html";
     }
 }
