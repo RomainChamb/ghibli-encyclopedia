c5d0604fe16e3f95383453a31f5464ab84c38c4d
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2025-11-06 23:33:58 +0100

    Apply module: init

--- monolith/src/main/webapp/app/app.route.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MovieList } from './movie-list'
-import { Home } from "./home";
-
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'movies', component: MovieList }
-];
