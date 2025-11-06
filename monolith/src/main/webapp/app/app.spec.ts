fe6daa877dee581d584b6f6d2db79dd1ac21fbc1
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2025-11-06 23:40:23 +0100

    Apply module: angular-core

---
+++ monolith/src/main/webapp/app/app.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed, waitForAsync } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
+import { App } from './app';
+
+describe('App Component', () => {
+  let comp: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [provideRouter([]), { provide: ComponentFixtureAutoDetect, useValue: true }],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(App);
+    comp = fixture.componentInstance;
+  });
+
+  describe('ngOnInit', () => {
+    it('should have appName', () => {
+      expect(comp.appName()).toBe('ghibliEncyclopedia');
+    });
+  });
+});
